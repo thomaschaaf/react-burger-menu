23a2feb7499ee4a6b85dc0cc2da5fe229af756c7
Thomas Schaaf <schaaf@komola.de>
2016-07-28 17:23:29 +0200

    ms is a pain

--- src/baseStyles.js
+++ src/baseStyles.js
@@ -11,7 +11,7 @@ let styles = {
       background: 'rgba(0, 0, 0, 0.3)',
       opacity: isOpen ? 1 : 0,
       WebkitTransform: isOpen ? '' : 'translate3d(-100%, 0, 0)',
-      msTransform: isOpen ? '' : 'translateX(-100%)',
+      '-ms-transform': isOpen ? '' : 'translateX(-100%)',
       transform: isOpen ? '' : 'translate3d(-100%, 0, 0)',
       WebkitTransition: isOpen ? 'opacity 0.3s' : 'opacity 0.3s, transform 0s 0.3s',
       transition: isOpen ? 'opacity 0.3s' : 'opacity 0.3s, transform 0s 0.3s',
@@ -33,7 +33,7 @@ let styles = {
     return {
       height: '100%',
       boxSizing: 'border-box',
-      msTransform: isOpen ? '' : right ? 'translateX(100%)' : 'translateX(-100%)',
+      '-ms-transform': isOpen ? '' : right ? 'translateX(100%)' : 'translateX(-100%)',
       WebkitTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
   		transform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
       WebkitTransition: 'all 0.5s',
